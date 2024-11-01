d39689f084d1614b34d666aa90ab9d5391bc8dfb
Murphybread <nargene@gmail.com>
2024-11-01 12:46:47 +0900

    style(commands): change footer introuduce

--- commands/dailyrecommend.js
+++ commands/dailyrecommend.js
@@ -100,7 +100,7 @@ module.exports = {
                 }],
                 timestamp: new Date(),
                 footer: {
-                    text: '작성자 [FE] 민찬 | 데이터는 6시간마다 업데이트 중'
+                    text: 'Developed by [FE] 민찬 | 데이터는 6시간마다 업데이트 중'
                 }
             };
 
